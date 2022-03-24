f8509d06726dee788ae485311818b9201e8f4b72
cathos <cathos@startmail.com>
2022-03-24 10:33:39 -0700

    heather's first commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pytest
 def always_returns_true():
     return True
 
+def heathers_function(test_data):
+    return not test_data
 
 def test_always_returns_true():
     assert always_returns_true()
