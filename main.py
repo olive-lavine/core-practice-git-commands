39e48563c33ebc3f787d0b6b6a6b2e36fe63e592
cathos <cathos@startmail.com>
2022-03-24 10:42:28 -0700

    add heather test

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def heathers_function(test_data):
 
 def test_always_returns_true():
     assert always_returns_true()
+
+def test_heathers_function():
+    test_data = False
+    test_return = heathers_function(test_data)
+    assert test_return == True
