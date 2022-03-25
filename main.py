7bc57fa5b398cca45c8f05c3cc3c82519438096f
jae <j.kerr.clayton@gmail.com>
2022-03-25 15:00:29 -0500

    jae's function

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def heathers_function(test_data):
 def test_always_returns_true():
     assert always_returns_true()
 
+def jae_function():
+    return True 
+
 def test_heathers_function():
     test_data = False
     test_return = heathers_function(test_data)
